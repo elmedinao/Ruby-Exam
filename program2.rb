9c135cc44c509e4a221636713e78dfd1b18c482b
elmedinao <eomerovic@cps.edu>
2014-03-26 19:28:21 +0000

    second program

---
+++ program2.rb
@@ -0,0 +1,11 @@
+array = ['English', 'French', 'Spanish']  
+puts 'I love '+ name[0]+'!' 
+puts 'Don\'t you?'
+a = gets.chomp
+puts 'I love '+name[1]  +'!'
+puts 'Don\'t you?'
+b = gets.chomp
+puts 'I love '+name[2]  +'!'
+puts 'Don\'t you?'
+c = gets.chomp
+
